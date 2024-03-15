eaeacb1c9565d79450db197c7f72f1e10f562214
functionalStoic <7215306+functionalStoic@users.noreply.github.com>
2024-03-15 08:35:41 -0600

    chore(deps): add project deps instead of per chapter

--- chapter-2/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const a = 1 + 2;
-const b = a + 3;
-const c = { apple: a, banana: b };
-const d = { ...c, cauliflower: 7 };
-const e = Object.keys(d);
-const f = e.map((key) => d[key]);
-console.log('f', f);
