7eb790eda2155759d445ae1bea8ee628319d0c98
functionalStoic <7215306+functionalStoic@users.noreply.github.com>
2024-11-13 08:56:21 -0700

    feat(code): nov 11 progress

--- chapter-6/scratchpad.ts
+++ chapter-6/scratchpad.ts
@@ -33,29 +33,41 @@ function chirp(bird: Bird): Bird {
  * Error TS2345: Argument of type 'Animal' is not assignable to parameter of type 'Bird'.
  * Property 'chirp' is missing in type 'Animal' but required in type 'Bird'.
  */
-chirp(new Animal());
+// chirp(new Animal());
 
 chirp(new Bird());
 chirp(new Crow());
 
 function clone(f: (b: Bird) => Bird): void {}
 
-function birdToBird(b: Bird): Bird {}
+function birdToBird(b: Bird): Bird {
+    return b;
+}
 clone(birdToBird); // Works as expected
 
-function birdToCrow(b: Bird): Crow {}
+function birdToCrow(b: Bird): Crow {
+    return new Crow();
+}
 clone(birdToCrow); // Works as expected
 
-function birdToAnimal(b: Bird): Animal {}
+function birdToAnimal(b: Bird): Animal {
+    return new Animal();
+}
 
 /**
  * Error TS2345: Argument of type '(b: Bird) => Animal' is not assignable to parameter of type '(b: Bird) => Bird'.
  * Type 'Animal' is not assignable to type 'Bird'.
  */
-clone(birdToAnimal);
+// clone(birdToAnimal);
 
-/**
- * -----------------
- *
- * -----------------
- */
+function animalToBird(a: Animal): Bird {
+    return new Bird();
+}
+clone(animalToBird); // Works as expected
+
+function crowToBird(c: Crow): Bird {
+    return new Bird();
+}
+// clone(crowToBird); // Error TS2345: Argument of type '(c: Crow) => Bird' is not assignable to parameter of type '(b: Bird) => Bird'.
+// Types of parameters 'c' and 'b' are incompatible.
+// Property 'caw' is missing in type 'Bird' but required in type 'Crow'.
